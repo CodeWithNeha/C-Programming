18eef906609a914007b9f182a29a015aa7828b07
Neha <codewithneha2020@gmail.com>
2022-01-05 10:00:08 +0530

    Arithmatic Operators

---
+++ Arithmatic_Operators.c
@@ -0,0 +1,25 @@
+//Operators:- 
+// 1. Arithmatic Operators:- +,-,*,/,%
+//5/2 = Qu=2 rem =1
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	// Addition + 
+	// int oscar = a+b;
+	// printf("sum of a and b = %d",oscar);
+	// Subtraction -
+	// int sub = a-b;
+	// printf("Diff of a and b = %d",sub);
+	// Multiplication *
+	// int mul = a*b;
+	// printf("multiplication of a and b = %d",mul);
+	// Division /
+	int div = a/b;
+	printf("a / b = %d",div);
+	// modulus operator % remainder
+	int mod = a%b;
+	printf("\na modulus b = %d",mod);
+
+}
