4d0d811dbdf7cf15f8c04d4e13c929fdaaa22964
Neha <codewithneha2020@gmail.com>
2022-01-05 10:04:02 +0530

    Conditional Operators

---
+++ Conditional_Operators.c
@@ -0,0 +1,12 @@
+// Conditional Operators:- 
+// (condition) ? exp3(true) : exp4(false); 
+#include<stdio.h>
+int main()
+{
+    // int a = 50,b= 30;  
+    // int c = (a!=b) ? a : b;
+    // printf("Greatest value = %d",c);
+    int a = 20, b =60;
+    int c = (a>=b) ? a : b;
+    printf("Greatest value = %d",c);
+}
