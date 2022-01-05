79b9e742b7a3ea35dc465392ee2cadf9646d267f
Neha <codewithneha2020@gmail.com>
2022-01-05 09:58:03 +0530

    Unformatted Input Output

---
+++ Unformatted_Input_Output.c
@@ -0,0 +1,13 @@
+//Unformatted Input/Output Functions
+//Character Input/Output
+//1. getchar() :- character_variable = getchar(); (Syntax)
+//2. putchar() :- putchar(character_variable);    (Syntax)
+//3. getche() :-  character_variable = getche();  (Syntax)
+//4. getch()  :-  character_variable = getch(): (Syntax)
+#include<stdio.h>
+int main()
+{
+  	char c;
+   c = getchar();
+  putchar(c);        //printf("%c",c);	
+}
