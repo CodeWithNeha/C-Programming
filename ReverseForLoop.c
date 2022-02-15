f102c48c684a83b7155229da054759668600b1cc
Neha <codewithneha2020@gmail.com>
2022-02-15 21:44:33 +0530

    Reverse Loop

---
+++ ReverseForLoop.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    for(int i = 10;i>=1;i--)   // 0>=1  -1>=1
+    {
+        // printf("\n %d",i);  
+        printf("\n Hello Sachman's World");
+    }
+}
---
+++ Sachman'sCode.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+int i=10;
+while(i<=10){
+    printf("\n %d",i);
+    i--;
+}
+}
